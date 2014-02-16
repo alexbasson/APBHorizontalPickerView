f3a1fef3e4931d3211fa132226f37eb075d60870
Alex Basson <alexbasson@gmail.com>
2014-02-16 15:16:38 -0500

    Create project

---
+++ APBHorizontalPickerView/APBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APBAppDelegate.h
+//  APBHorizontalPickerView
+//
+//  Created by Alex on 2/16/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
